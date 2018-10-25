b928d435aec7c7c3f345142e6df67a62a8869905
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-25 14:32:24 +0530

    Add ARIA role to view details link

--- js/main.js
+++ js/main.js
@@ -179,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'View Details');
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
