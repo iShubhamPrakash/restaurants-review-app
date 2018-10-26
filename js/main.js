df80517ad7d467726822a87120f1a53b5c944196
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-27 00:23:52 +0530

    Fix: Use less iimportant <h> tag for semantic reason in case of "name"

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
